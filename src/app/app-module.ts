12663e9f18f7199f4ced4dfaa531b2e158ea8c91
AngelChame <chapperzz3@gmail.com>
2025-10-28 20:10:43 -0600

    music player structure

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,22 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SongInfo } from './song-info/song-info';
-import { SecondChild } from './second-child/second-child';
+import { AudioController } from './audio-controller/audio-controller';
+import { Playlist } from './playlist/playlist';
+import { Player } from './player/player';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    App,
-    SongInfo,
-    SecondChild
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection()
-  ],
-  bootstrap: [App]
-})
-export class AppModule { }
+
+
+
+
+
